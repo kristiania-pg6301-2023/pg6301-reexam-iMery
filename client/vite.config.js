be1519339fd20aa7a2de67e859ce64d8990aa87f
Mariam Kajo <Maryamkajo2001@hotmail.com>
2025-02-27 22:34:08 +0100

    Deploy entire project to Heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // Ensures Vite runs on a port Heroku expects
   },
+  build: {
+    outDir: 'dist', // Ensure it builds to the right directory
+  }
 });
