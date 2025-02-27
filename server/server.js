09ee2965d9ba4fc9022e5aa14964cba51ef0cf50
Mariam Kajo <Maryamkajo2001@hotmail.com>
2025-02-27 23:31:30 +0100

    Fix case sensitivity issue for Heroku

--- server/server.js
+++ server/server.js
@@ -26,6 +26,12 @@ app.use("/api", loginApi);
 app.use("/api/posts", postRoutes, commentRoutes);
 app.use(express.static("../client/dist"));
 
+app.use(express.static(path.join(__dirname, "dist"))); 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 const PORT = process.env.PORT || 8000; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
